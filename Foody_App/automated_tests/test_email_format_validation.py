324358242d8e77733f29ebaff82cd43bd8cecf0a
SlaviHristovIvanov <92828826+SlaviHristovIvanov@users.noreply.github.com>
2025-03-07 15:44:41 +0200

    Update test_email_format_validation.py

--- Foody_App/automated_tests/test_email_format_validation.py
+++ Foody_App/automated_tests/test_email_format_validation.py
@@ -1,17 +1,21 @@
 import pytest
+import re
 
 email_data = [
-    ('some.email@gmail.com', True),
-    ('invalid-email.com', False),
-    ('@invalid.com', False),
-    ('some.email@com', False)
+    ('some.email@gmail.com', True),        # Valid email
+    ('invalid-email.com', False),          # Invalid email (no '@')
+    ('@invalid.com', False),               # Invalid email (no username before '@')
+    ('some.email@com', False),             # Invalid email (no top-level domain)
+    ('some.email@subdomain.domain.com', True),  # Valid email with subdomain
+    ('another.valid.email@domain.co.uk', True), # Valid email with multiple TLD parts
+    ('test@domain', False),                # Invalid email (no TLD)
+    ('test@.com', False),                  # Invalid email (TLD starts with a dot)
 ]
 
-@pytest.mark.parametrize('email, expected_result', email_data)
+email_regex = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
 
+@pytest.mark.parametrize('email, expected_result', email_data)
 def test_email_validation(email, expected_result):
-    import re
-    email_regex = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
     if re.match(email_regex, email):
         assert expected_result == True
     else:
